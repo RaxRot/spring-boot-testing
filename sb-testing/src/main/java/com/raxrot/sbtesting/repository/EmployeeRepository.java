f6bdcbeab72c00b6f30fb710c5d34bd57477c5f7
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-18 18:08:30 +0100

    add some req to repo

--- sb-testing/src/main/java/com/raxrot/sbtesting/repository/EmployeeRepository.java
+++ sb-testing/src/main/java/com/raxrot/sbtesting/repository/EmployeeRepository.java
@@ -2,6 +2,14 @@ package com.raxrot.sbtesting.repository;
 
 import com.raxrot.sbtesting.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+
+    @Query("select e from Employee e where e.firstName=:firstName and e.lastName=:lastName")
+    Employee findByJPQL(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
