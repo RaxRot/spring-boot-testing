26853f45f69edaf13f0a7dfe61f71ae209fa6ad7
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-19 16:59:11 +0100

    add EmployeeServiceImpl CRUD methods

--- sb-testing/src/main/java/com/raxrot/sbtesting/service/EmployeeServiceImpl.java
+++ sb-testing/src/main/java/com/raxrot/sbtesting/service/EmployeeServiceImpl.java
@@ -1,22 +1,67 @@
 package com.raxrot.sbtesting.service;
 
+import com.raxrot.sbtesting.dto.EmployeeDTO;
 import com.raxrot.sbtesting.entity.Employee;
+import com.raxrot.sbtesting.exception.ApiException;
 import com.raxrot.sbtesting.repository.EmployeeRepository;
+import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+    private final ModelMapper modelMapper;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
         this.employeeRepository = employeeRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
-    public Employee saveEmployee(Employee employee) {
-        Optional<Employee> employeeOptional=employeeRepository.findByEmail(employee.getEmail());
-        if(employeeOptional.isPresent()){
-            throw new RuntimeException("Employee already exists");
+    public EmployeeDTO saveEmployee(EmployeeDTO employee) {
+        Optional<Employee> employeeOptional = employeeRepository.findByEmail(employee.getEmail());
+
+        if (employeeOptional.isPresent()) {
+            throw new ApiException("Employee already exists with email: " + employee.getEmail());
         }
-        return employeeRepository.save(employee);
+
+        Employee employeeEntity = modelMapper.map(employee, Employee.class);
+        Employee savedEmployee = employeeRepository.save(employeeEntity);
+        return modelMapper.map(savedEmployee, EmployeeDTO.class);
+    }
+
+    @Override
+    public List<EmployeeDTO> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(employee -> modelMapper.map(employee, EmployeeDTO.class)).collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeById(Long id) {
+        Employee foundEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ApiException("Employee not found with id: " + id));
+        return modelMapper.map(foundEmployee, EmployeeDTO.class);
+    }
+
+    @Override
+    public EmployeeDTO updateEmployee(Long id, EmployeeDTO employeeDTO) {
+        Employee existing = employeeRepository.findById(id)
+                .orElseThrow(() -> new ApiException("Employee not found with id: " + id));
+
+        existing.setFirstName(employeeDTO.getFirstName());
+        existing.setLastName(employeeDTO.getLastName());
+        existing.setEmail(employeeDTO.getEmail());
+
+        Employee updated = employeeRepository.save(existing);
+        return modelMapper.map(updated, EmployeeDTO.class);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        Employee existing = employeeRepository.findById(id)
+                .orElseThrow(() -> new ApiException("Employee not found with id: " + id));
+
+        employeeRepository.delete(existing);
     }
 }
