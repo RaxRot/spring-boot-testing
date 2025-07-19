18cf80bad8ac20a9a5a0583d94e9f6635dcd74ea
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-19 16:58:57 +0100

    add EmployeeService CRUD methods

--- sb-testing/src/main/java/com/raxrot/sbtesting/service/EmployeeService.java
+++ sb-testing/src/main/java/com/raxrot/sbtesting/service/EmployeeService.java
@@ -1,7 +1,13 @@
 package com.raxrot.sbtesting.service;
 
-import com.raxrot.sbtesting.entity.Employee;
+import com.raxrot.sbtesting.dto.EmployeeDTO;
+
+import java.util.List;
 
 public interface EmployeeService {
-    Employee saveEmployee(Employee employee);
+    EmployeeDTO saveEmployee(EmployeeDTO employee);
+    List<EmployeeDTO>getAllEmployees();
+    EmployeeDTO getEmployeeById(Long id);
+    EmployeeDTO updateEmployee(Long id,EmployeeDTO employee);
+    void deleteEmployee(Long id);
 }
